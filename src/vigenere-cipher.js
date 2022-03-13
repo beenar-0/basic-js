5e6be3d2ee1d985b5e24f0a6bf9e2ac84f12eb29
beenar-0 <dncastledn@gmail.com>
2022-03-13 05:08:46 +0300

    feat: added vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect
+    this.alphabet = []
+    for (let i = 65; i <= 90; i++) {
+      this.alphabet.push(String.fromCharCode(i))
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  reg = /[A-Z]/g
+
+  genereteKey(message, key) {
+    let count = 0
+    return message.replace(this.reg, (item) => {
+      let rez = key[count++]
+      if (count === key.length) count = 0
+      return rez.toUpperCase()
+    })
+  }
+  encrypt(message, key) {
+    if (typeof message !== 'string' || typeof key !== "string") throw new Error('Incorrect arguments!')
+    let reg = /[A-Z]/g
+    message = message.toUpperCase()
+    key = this.genereteKey(message, key)
+    message = message.replace(reg, (item, index) => {
+      let newAlphabet = this.alphabet.slice(this.alphabet.indexOf(key[index])).concat(this.alphabet).slice(0, 26)
+      return newAlphabet[this.alphabet.indexOf(item)]
+    })
+    if (this.isDirect) return message
+    else return message.split('').reverse().join('')
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (typeof encryptedMessage !== 'string' || typeof key !== "string") throw new Error('Incorrect arguments!')
+    encryptedMessage = encryptedMessage.toUpperCase()
+    key = this.genereteKey(encryptedMessage, key)
+    encryptedMessage = encryptedMessage.replace(this.reg, (item,index)=>{
+      let newAlphabet = this.alphabet.slice(this.alphabet.indexOf(key[index])).concat(this.alphabet).slice(0, 26)
+      return this.alphabet[newAlphabet.indexOf(item)]
+    })
+    if (this.isDirect) return encryptedMessage
+    else return encryptedMessage.split('').reverse().join('')
   }
 }
 
